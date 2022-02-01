dfe4bd16f26381d07fbcea61411c6661af68ef58
Jakub Strzelecki <arutorek@gmail.com>
2022-02-01 12:22:08 +0100

    show list complete

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,39 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_most_rated_shows(page, order_by='rating', order_direction='desc'):
+    if order_by is None:
+        order_by = 'rating'
+    if order_direction is None:
+        order_direction = 'desc'
+    return data_manager.execute_select(
+        f'''
+        SELECT id, title, year, runtime, rating, trailer, homepage 
+        FROM shows
+        ORDER BY {order_by} {order_direction}
+        LIMIT 15
+        OFFSET ({page}-1)*15;
+        '''
+    )
+
+
+def get_genres_from_show(show_id):
+    return data_manager.execute_select(
+        f"""
+        SELECT genres.name, show_id
+        FROM show_genres
+        JOIN genres ON genres.id = genre_id
+        WHERE show_id = {show_id}
+        """
+    )
+
+
+def get_show_count():
+    return data_manager.execute_select(
+        f"""
+        SELECT COUNT(*) AS show_count 
+        FROM shows
+        """
+    )
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 import math
 from dotenv import load_dotenv
+import utils
+
 
 load_dotenv()
 app = Flask('codecool_series')
@@ -15,6 +17,37 @@ def index():
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+
+@app.route('/shows')
+@app.route('/shows/<int:page>')
+@app.route('/shows/most-rated')
+@app.route('/shows/most-rated/<int:page>')
+def most_rated(page=1):
+    order_by, order_direction = 'rating', 'desc'
+    if request.args:
+        order_by = list(dict(request.args).keys())[0]
+        order_direction = dict(request.args)[order_by]
+    number_of_shows = queries.get_show_count()[0]['show_count']
+    page_count = math.ceil(number_of_shows/15)
+    shown_pages = utils.check_pages(page, page_count)
+    most_rated_shows = queries.get_most_rated_shows(page, order_by, order_direction)
+    for show in most_rated_shows:
+        genres = []
+        show_id = show['id']
+        get_genres = queries.get_genres_from_show(show_id)
+        for genre in get_genres:
+            genres.append(genre['name'])
+        show['genres'] = genres
+    return render_template('most-rated.html', shows=most_rated_shows,
+                           shown_pages=shown_pages, page_count=page_count,
+                           page=page, url=request.url, order_by=order_by,
+                           order_direction=order_direction)
+
+
+@app.route('/show/<id>')
+def show_details():
+    return render_template()
 
 
 def main():
